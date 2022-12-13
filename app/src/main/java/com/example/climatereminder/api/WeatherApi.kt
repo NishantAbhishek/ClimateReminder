71db5a64509473c2dc7c94542b9e4f2640ff8777
nishant2548@gmail.com <NishantIsCool1>
2022-12-13 18:46:31 +1100

    Project SetUp

---
+++ app/src/main/java/com/example/climatereminder/api/WeatherApi.kt
@@ -0,0 +1,4 @@
+package com.example.climatereminder.api
+
+interface WeatherApi {
+}
---
+++ app/src/main/java/com/example/climatereminder/util/Constants.kt
@@ -0,0 +1,12 @@
+package com.example.climatereminder.util
+
+class Constants {
+
+    companion object{
+        const val API_KEY = "9ad94b2e4604473eba471536221312 "
+        const val BASE_URL = ""
+
+    }
+
+
+}
